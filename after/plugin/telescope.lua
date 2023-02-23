0196708238ef33dbdf521b4d91d2c5b8d111bbfb
Harvey Nighy <harveynighy@talktalk.net>
2023-02-23 10:48:22 +0000

    Telescope/Tree Config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,7 @@ vim.keymap.set('n', '<C-p>pf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+require("telescope").setup{
+    file_ignore_patterns = { "node%_modules/.*" }
+}
--- after/plugin/tree.lua
+++ after/plugin/tree.lua
@@ -20,7 +20,4 @@ require("nvim-tree").setup({
     renderer = {
         group_empty = true,
     },
---    filters = {
---        dotfiles = true,
---    },
 })
